a36f47a605e2ed42ee6e188e5cb257105ef2e726
alpayariyak <aariyak@wpi.edu>
2022-11-12 20:32:18 +0000

    Progress

---
+++ run7.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH --gres=gpu:1
+#SBATCH -t 20:00:00
+#SBATCH --mem 8G
+
+python -u main.py --train_dqn --n_episodes=5000000 --decay_end=80000 --epsilon_end=0.1 --optimize_interval=4 --target_update_interval=5000 --evaluate_interval=10000 --buffer_size=400000 --initialize_weights=False --clip_grad=True > test5_short.out
--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH -N 1
 #SBATCH -c 4
-#SBATCH -t 0:10:00
+#SBATCH --gres=gpu:1
+#SBATCH -t 0:3:00
 #SBATCH --mem 8G
 
-python -u main.py --train_dqn --n_episodes=30000 --decay_end=10000 --epsilon_end=0.025 --learning_rate=0.00005 --optimize_interval=4 --target_update_interval=5000 --evaluate_interval=10000 --buffer_size=10000 --initialize_weights=False> test_run.out
+python -u main.py --test_dqn > test_out.out
