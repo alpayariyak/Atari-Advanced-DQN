cbb6ac4c7120962ba9041da8f2343dfac071eef7
alpayariyak <aariyak@wpi.edu>
2022-11-13 00:32:07 +0000

    cleaning

--- run3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#SBATCH -N 1
-#SBATCH -c 4
-#SBATCH --nodelist=whitehill-0-01
-#SBATCH -t 08:00:00
-#SBATCH --mem 8G
-
-python -u main.py --train_dqn --n_episodes=500000 --decay_end=80000 --epsilon_end=0.025 --learning_rate=0.00005 --optimize_interval=4 --target_update_interval=5000 --evaluate_interval=10000 --buffer_size=100000 --initialize_weights=False> test3.out
